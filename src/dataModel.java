618019470dd7c2080bfb4dbb21793153f0bf4073
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-19 06:51:14 +0900

    Create dataModel.java

---
+++ src/dataModel.java
@@ -0,0 +1,10 @@
+import org.springframework.ui.Model;
+
+
+
+public String hello (Model model) {
+  
+        model.addAttribute("USERNAME", "kimkimkim"); //key : value
+        return "hello"; //mostache
+}
+
