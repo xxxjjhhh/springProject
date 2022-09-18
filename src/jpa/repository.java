37e482f0eb0603a238ee83fed7e373489094183a
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-19 08:33:02 +0900

    Create repository.java

---
+++ src/jpa/repository.java
@@ -0,0 +1,11 @@
+//패키지 생성 src/main/java/com.example.project1/repository
+//클래스(인터페이스 : Interface) 생성 HelloRepository.java (jpa crud 클래스를 상속 받기 위해서 인터페이스로 생성)
+
+package com.example.project1.repository;
+
+import org.springframework.data.repositiry.CrudRepository;
+
+public interface HelloRepository extends CrudRepository<HelloEntity, Long> { //<entity이름, id값데이터타입>
+  
+    
+}
