755027c539f6311e6435d1c60161e77e8bbac571
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2022-09-19 07:39:49 +0900

    Create redirect.java

---
+++ src/redirect.java
@@ -0,0 +1,31 @@
+package com.example.project1.cotroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class hello {
+
+    @GetMapping("/hello") //redir sender
+    public String hello (RedirectAttributes redir) {
+        
+        redir.addAttribute("key", "value");
+        return "redirect:/post1";
+      //오류시 / 제거
+    }
+  
+  
+    @GetMapping("/post1") //redir receiver
+    public String hello (@RequestParam("pageNum") String key) {
+        
+        redir.addAttribute("key", "value");
+        return "redirect:/post1";
+      //오류시 / 제거
+    }
+  
+
+}
+
+//오류시 @RequestMapping
+//RedirAttributes의 경우 파라미터 정보 URI에 노출됨
